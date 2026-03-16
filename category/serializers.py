8237293fbd91070c462879d009cfb6538fc77570
akshe <akshey.panika@gmail.com>
2026-03-16 17:36:14 +0530

    add cloudinary

--- category/serializers.py
+++ category/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.conf import settings
 from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -12,9 +13,24 @@ class CategorySerializer(serializers.ModelSerializer):
         representation = super().to_representation(instance)
         request = self.context.get("request")
 
-        if instance.category_image and request:
-            representation["category_image"] = request.build_absolute_uri(instance.category_image.url)
+        if instance.category_image:
+            url = instance.category_image.url
+            # If in local development and the URL is relative, make it absolute
+            if settings.DEBUG and not url.startswith("http"):
+                url = request.build_absolute_uri(url)
+            representation["category_image"] = url
         else:
             representation["category_image"] = None
 
-        return representation
+        return representation  
+
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     request = self.context.get("request")
+
+    #     if instance.category_image and request:
+    #         representation["category_image"] = request.build_absolute_uri(instance.category_image.url)
+    #     else:
+    #         representation["category_image"] = None
+
+    #     return representation
