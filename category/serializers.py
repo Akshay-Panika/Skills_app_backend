3d4f8c1ca574dc0c7d96379840aea864d9fdabf1
akshe <akshey.panika@gmail.com>
2026-03-16 10:59:51 +0530

    mini update

--- category/serializers.py
+++ category/serializers.py
@@ -8,12 +8,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ["id", "category_name", "category_image"]
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        request = self.context.get("request")
-        if instance.category_image and request:
-            # Build full URL for the uploaded image
-            representation["category_image"] = request.build_absolute_uri(instance.category_image.url)
-        else:
-            representation["category_image"] = None
-        return representation
+def to_representation(self, instance):
+    representation = super().to_representation(instance)
+    request = self.context.get("request")
+    if instance.category_image and request:
+        representation["category_image"] = request.build_absolute_uri(instance.category_image.url)
+    else:
+        representation["category_image"] = None
+    return representation
