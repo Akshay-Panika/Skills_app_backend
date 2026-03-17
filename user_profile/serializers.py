aa01c332159a7419061a1f808467fac7286c8456
akshe <akshey.panika@gmail.com>
2026-03-17 14:36:28 +0530

    add cloudinary

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -4,7 +4,14 @@ from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
+    user_image = serializers.SerializerMethodField()
+
     class Meta:
         model = UserProfile
         fields = "__all__"
         read_only_fields = ["user", "user_phone"]
+
+    def get_user_image(self, obj):
+        if obj.user_image:
+            return obj.user_image.url  
+        return None
