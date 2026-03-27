564d0140dd55c888e66ffc3bcf74f24193404aae
akshe <akshey.panika@gmail.com>
2026-03-27 22:49:27 +0530

    add cloudinary

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ChatMessagesView, UserChatsView
+from .views import ChatMessagesView, UserChatsView, CreateChatRoomView
 
 urlpatterns = [
     path("chat/messages/<int:chat_room_id>/", ChatMessagesView.as_view()),
     path("chat/user/<int:user_id>/", UserChatsView.as_view()),
+    path("chat/create/", CreateChatRoomView.as_view()),
 ]
