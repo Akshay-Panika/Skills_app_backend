24121bd97f9e13c689e0a28736aaa872d3e30d1e
akshe <akshey.panika@gmail.com>
2026-04-24 01:21:39 +0530

    fix chat migrations reset

--- chat/urls.py
+++ chat/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CreateChatRoomView, ChatHistoryView, ChatRoomListView
 
 urlpatterns = [
-    path("create-room/", CreateChatRoomView.as_view(), name="create-room"),
+    path("chat/create-room/", CreateChatRoomView.as_view(), name="create-room"),
 
-    path("rooms/<int:user_id>/", ChatRoomListView.as_view(), name="chat-rooms"),
+    path("chat/rooms/<int:user_id>/", ChatRoomListView.as_view(), name="chat-rooms"),
 
-    path("history/<int:room_id>/", ChatHistoryView.as_view(), name="chat-history"),
+    path("chat/history/<int:room_id>/", ChatHistoryView.as_view(), name="chat-history"),
 ]
